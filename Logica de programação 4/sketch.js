030cf90aeaddeb56f3b060ed2b4c3268839b26ef
Jessica Santos <jessica16hs@gmail.com>
2022-11-14 14:48:33 -0300

    mais carrinhos, sobe e desce vaquinha

--- Logica de programação 4/sketch.js
+++ Logica de programação 4/sketch.js
@@ -1,48 +1,19 @@
-let imagemDaEstrada;
-let imagemDoAtor;
-let imagemCarro;
-
-//carro
-let xCarro = 600;
-
-//ator
-let yAtor = 366;
-
-function preload(){
-    imagemDaEstrada = loadImage("imagens/estrada.png");
-    imagemDoAtor = loadImage('imagens/ator-1.png');
-    imagemCarro = loadImage("imagens/carro-1.png");
-}
-
-function setup(){
+function setup() {
     createCanvas(500, 400);
-}
-
-function draw(){
+  
+  }
+  
+  function draw() {
     background(imagemDaEstrada);
     mostraAtor();
     mostraCarro();
     movimentaCarro();
     movimentaAtor();
-}
+    voltaPosicaoInicialDoCarro();
+    verificaColisao();
+    incluiPontos();
+    marcaPonto();
+    
+  }
+  
 
-function mostraAtor(){
-    image(imagemDoAtor, 100, yAtor, 30, 30);
-}
-
-function mostraCarro(){
-    image(imagemCarro, xCarro, 40, 50, 40);
-}
-
-function movimentaCarro(){
-  xCarro  -= 2;
-}
-
-function movimentaAtor(){
-    if(keyIsDown(UP_ARROW)){
-      yAtor -= 3;
-    }
-    if(keyIsDown(DOWN_ARROW)){
-        yAtor += 3;
-      }
-}
