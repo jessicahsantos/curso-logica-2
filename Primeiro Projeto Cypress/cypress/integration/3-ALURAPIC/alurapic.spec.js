6d4a1d38c5eaa2be5d6e54e394dd9467fbb8ba17
Jessica Santos <jessica16hs@gmail.com>
2022-12-02 11:10:40 -0300

    Alteração

--- Primeiro Projeto Cypress/cypress/integration/3-ALURAPIC/alurapic.spec.js
+++ Primeiro Projeto Cypress/cypress/integration/3-ALURAPIC/alurapic.spec.js
@@ -1,7 +1,6 @@
 describe('Login e registro de usuarios alura pic', () => {
 
     beforeEach(() => {
-
         cy.visit("https://alura-fotos.herokuapp.com");
     })
 
@@ -31,5 +30,18 @@ describe('Login e registro de usuarios alura pic', () => {
         cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
         
     })
+   
+    it.only('fazer login de usuario valido', () => {
+        cy.login('flavio', '123')
+        cy.contains('a', '(Logout').should('be.visible'); 
+    })
+
+    it.only('fazer login de usuario invalido', () => {
+       cy.login('jacqueline', '1234')
+       cy.on('window:alert', (str) => {
+          expect(str).to.equal('Invalid user name or password')
+       })
+    })
+    
 
 })
