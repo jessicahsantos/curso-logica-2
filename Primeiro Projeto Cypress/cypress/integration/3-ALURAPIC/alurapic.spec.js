f97997cf08b8a4794571efc0fd75c243161e753c
Jessica Santos <jessica16hs@gmail.com>
2022-12-03 08:38:03 -0300

    dados

--- Primeiro Projeto Cypress/cypress/integration/3-ALURAPIC/alurapic.spec.js
+++ Primeiro Projeto Cypress/cypress/integration/3-ALURAPIC/alurapic.spec.js
@@ -1,10 +1,13 @@
+const { it } = require("mocha");
+
 describe('Login e registro de usuarios alura pic', () => {
 
     beforeEach(() => {
-        cy.visit("https://alura-fotos.herokuapp.com");
-    })
+      cy.visit('https://alura-fotos.herokuapp.com')
 
-    it("verifica mensagens validacao", () => {
+     })
+
+    it('verifica mensagens validacao', () => {
         cy.contains('a', 'Register now').click();
         cy.contains('button', 'Register').click();
         cy.contains('ap-vmessage', 'Email is required!').should('be.visible');
@@ -14,12 +17,12 @@ describe('Login e registro de usuarios alura pic', () => {
         cy.contains('ap-vmessage', 'Full name is required!').should('be.visible');
     })
 
-    it("verifica mensagens de email invalido", () => {
+    it('verifica mensagens de email invalido', () => {
         cy.contains('a', 'Register now').click();
         cy.contains('button', 'Register').click();
-        cy.get('input[formcontrolname="email"]').type('jessica');
+        cy.get('input[formcontrolname="email"]').type('jacqueline');
         cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
-        
+     
     })
 
     it('verifica mensagens de senha com menos de 8 caracteres', () => {
@@ -28,20 +31,33 @@ describe('Login e registro de usuarios alura pic', () => {
         cy.get('input[formcontrolname="password"]').type('123');
         cy.contains('button', 'Register').click();
         cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
-        
-    })
-   
-    it.only('fazer login de usuario valido', () => {
-        cy.login('flavio', '123')
-        cy.contains('a', '(Logout').should('be.visible'); 
+     
     })
 
-    it.only('fazer login de usuario invalido', () => {
-       cy.login('jacqueline', '1234')
-       cy.on('window:alert', (str) => {
-          expect(str).to.equal('Invalid user name or password')
-       })
+    it('fazer login de usuario valido', () => {
+        cy.login('flavio', '123');
+        cy.contains('a', '(Logout)').should('be.visible');
+    })
+
+    it('fazer login de usuario invalido', () => {
+        cy.login('jacqueline', '1234');
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('Invalid user name or password');
+        })
+    })
+    const usuarios = require('../../fixtures/usuario.json');
+    usuarios.forEach(usuario => {
+
+        it.only(`registra novo usuario ${usuario.userName}`,() => {
+            cy.contains('a', 'Register now').click();
+            cy.contains('button', 'Register').click();
+            cy.get('input[formcontrolname="email"]').type(usuario.email);
+            cy.get('input[formcontrolname="fullName"]').type(usuario.fullName);
+            cy.get('input[formcontrolname="userName"]').type(usuario.userName);
+            cy.get('input[formcontrolname="password"]').type(usuario.password);
+            cy.contains('button', 'Register').click();
+        })
     })
     
-
 })
+
